2aeb83345f67629b682125b075bc8ed12ef22e18
mandar-deshpande <mandar.deshpande@gmail.com>
2017-01-27 11:11:12 +0530

    Mandar commited on master to resolve bugs

---
+++ src/test/java/com/morningstar/Calculators/MockTests.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package com.morningstar.Calculators;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author mdeshpa
+ *
+ */
+public class MockTests {
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#CalcApplication()}.
+	 */
+	@Test
+	public void testCalcApplication() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#add(double, double)}.
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#subtract(double, double)}.
+	 */
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#multiply(double, double)}.
+	 */
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#divide(double, double)}.
+	 */
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#expone(double)}.
+	 */
+	@Test
+	public void testExpone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#nroot(double, double)}.
+	 */
+	@Test
+	public void testNroot() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#nthroot(double, double, double)}.
+	 */
+	@Test
+	public void testNthroot() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#qroot(double, double, double)}.
+	 */
+	@Test
+	public void testQroot() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#logc(double)}.
+	 */
+	@Test
+	public void testLogc() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#display_PI()}.
+	 */
+	@Test
+	public void testDisplay_PI() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#sin(double)}.
+	 */
+	@Test
+	public void testSin() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#cos(double)}.
+	 */
+	@Test
+	public void testCos() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#mod(double, double)}.
+	 */
+	@Test
+	public void testMod() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.morningstar.calculators.CalcApplication#GeoTan(double)}.
+	 */
+	@Test
+	public void testGeoTan() {
+		fail("Not yet implemented");
+	}
+
+}
